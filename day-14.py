eae564a8e0cd4adf8d4e6208f2fbb5e7be339efc
Rithish1100 <142746483+Rithish1100@users.noreply.github.com>
2026-02-24 19:26:34 +0530

    day-14

---
+++ day-14.py
@@ -0,0 +1,44 @@
+import random
+import gamedata as g
+import art1 as art
+cans=0
+score=0
+a=0
+while(cans>=0):
+
+    print(art.a)
+    if(a==0):
+        a=random.randint(0,10)
+    b=random.randint(0,10)
+
+    if(cans>=0):
+        print("Thats correct ,Current score:",score)
+
+    if(a!=b):
+        print("compare A:",g.data[a]['name'],g.data[a]['description'],g.data[a]['country'])
+        print(art.b)
+        print("compare B:",g.data[b]['name'],g.data[b]['description'],g.data[b]['country'])
+        choice=input("how has more followers Type 'A' or 'B':")
+        afol=(g.data[a]['follower_count'])
+        bfol=(g.data[b]['follower_count'])
+        if(choice=='A'):
+            if(afol>bfol):
+                print("correct answer")
+                a=b
+                score+=1
+            else:
+                cans=-1
+                print(art.a)
+                print("better luck next time,Your score",score)
+           
+        elif(choice=='B'):
+            if(bfol>afol):
+                print("correct answer")
+                a=b
+                score+=1
+            else:
+                cans=-1
+                print(art.a)
+                print("Better luck next time",score)
+           
+    
