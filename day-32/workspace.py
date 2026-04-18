66448a6e0283875d5a7f5e3a8f5a2a0a9ad6464f
Rithish1100 <142746483+Rithish1100@users.noreply.github.com>
2026-04-18 15:13:47 +0530

    Disable email sending feature in workspace.py
    Comment out email functionality and related imports.

--- day-32/workspace.py
+++ day-32/workspace.py
@@ -15,25 +15,25 @@
 # month=now.month
 # date_of_birth=dt.datetime(year=2000,month=12,day=13,hour=3,minute=33)
 # print(date_of_birth)
-import smtplib
-import datetime as dt
-import random
+# import smtplib
+# import datetime as dt
+# import random
 
-list=[]
-my_email="rithishtest@gmail.com"
-password="edgo wxmh azvg xdtm"
-now=dt.datetime.now()
+# list=[]
+# my_email="rithishtest@gmail.com"
+# password="edgo wxmh azvg xdtm"
+# now=dt.datetime.now()
 
-today=now.weekday()
+# today=now.weekday()
 
-with open("quotes.txt") as quotes:
-    list=quotes.readlines()
-    random_quote=random.choice(list)
+# with open("quotes.txt") as quotes:
+#     list=quotes.readlines()
+#     random_quote=random.choice(list)
 
-if today==0:
-    print(today)
-    with smtplib.SMTP("smtp.gmail.com",587) as connection:
-        connection.starttls()
-        connection.login(user=my_email,password=password)
-        connection.sendmail(from_addr=my_email,to_addrs="rithishtestemail@yahoo.com",
-                        msg=f"Subject:Quote of Monday\n\n{random_quote}")
+# if today==0:
+#     print(today)
+#     with smtplib.SMTP("smtp.gmail.com",587) as connection:
+#         connection.starttls()
+#         connection.login(user=my_email,password=password)
+#         connection.sendmail(from_addr=my_email,to_addrs="rithishtestemail@yahoo.com",
+#                         msg=f"Subject:Quote of Monday\n\n{random_quote}")
