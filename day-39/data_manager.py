6e0a04cdc0e1643ea647437bc462405a688ddb5a
Rithish C Gouda <rithishcgouda.com>
2026-04-26 18:15:07 +0530

    day-39

---
+++ day-39/data_manager.py
@@ -0,0 +1,32 @@
+import requests
+import os
+from requests.auth import HTTPBasicAuth
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SHEETY_ENDPOINT="https://api.sheety.co/67caa158f9c0fd125abbccbd1b2cf45c/myFlightDeals/prices"
+class DataManager:
+    def __init__(self):
+        self._user=os.environ["SHEETY_USERNAME"]
+        self._password=os.environ["SHEETY_PASSWORD"]
+        self._authorization=HTTPBasicAuth(self._user,self._password)
+        self.destination_data={}
+
+    def get_destination_data(self):
+        response=requests.get(url=SHEETY_ENDPOINT,auth=self._authorization)
+        data=response.json()
+        self.destination_data=data["prices"]
+        return self.destination_data
+    
+    def update_lowest_price(self,row_id,new_price):
+        new_data={
+            "price":{
+                "lowest_price":new_price
+            }
+        }
+        requests.put(
+            url=SHEETY_ENDPOINT,
+            json=new_data,
+            auth=self.authorizatiion,
+        )
---
+++ day-39/main.py
@@ -0,0 +1,34 @@
+import requests_cache
+from data_manager import DataManager
+from pprint import pprint
+from datetime import datetime,timedelta
+from dateutil.relativedelta import relativedelta
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+
+requests_cache.install_cache("flight_cache",
+urls_expire_after={
+    "*.sheety.co*":requests_cache.DO_NOT_CACHE,
+    "*":3600,
+}
+)
+data_manager=DataManager()
+flight_search=FlightSearch()
+sheet_data=data_manager.get_destination_data()
+now=datetime.now()
+tommorow=now+timedelta(days=1)
+six_months_from_today=now+relativedelta(months=6)
+
+ORIGIN_CITY_IATA="LHR"
+for destination in sheet_data:
+    pprint(f"Getting flights for{destination['city']}")
+    flights=flight_search.check_flights(ORIGIN_CITY_IATA,destination["iataCode"],from_time=tommorow,to_time=six_months_from_today)
+
+# pprint(flight_data)
+# pprint(sheet_data)
+cheapest_flight=find_cheapest_flight(flights,return_date=six_months_from_today.strftime("%Y-%m-%d"))
+pprint(f"{destination['city']}:GBP{cheapest_flight.price}")
+
+if cheapest_flight.price!="N/A" and cheapest_flight.price<sheet_data[0]["lowestPrice"]:
+    pprint(f"lower price flight found to{destination['city']}!")
+    data_manager.update_lowest_price(destination["id"],cheapest_flight.price)
